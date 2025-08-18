640e14c485ebc8ec1482c791b5b17958fd15e5c4
Jazer <saltanhayrliev2006@gmail.com>
2025-08-18 23:03:20 +0300

    добавил телеграм токен и appsettings json-ы

--- VideoDownloader/Program.cs
+++ VideoDownloader/Program.cs
@@ -1,6 +1,13 @@
 using VideoDownloader;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+builder.Configuration
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile("appsettings.Secret.json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables();
+
 builder.Services.AddHostedService<Worker>();
 
 var host = builder.Build();
--- VideoDownloader/Worker.cs
+++ VideoDownloader/Worker.cs
@@ -3,10 +3,12 @@ namespace VideoDownloader;
 public class Worker : BackgroundService
 {
     private readonly ILogger<Worker> _logger;
+    private readonly IConfiguration _configuration;
 
-    public Worker(ILogger<Worker> logger)
+    public Worker(ILogger<Worker> logger, IConfiguration configuration)
     {
         _logger = logger;
+        _configuration = configuration;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -16,6 +18,7 @@ public class Worker : BackgroundService
             if (_logger.IsEnabled(LogLevel.Information))
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                _logger.LogInformation($"{_configuration["TelegramToken"]}");
             }
 
             await Task.Delay(1000, stoppingToken);
