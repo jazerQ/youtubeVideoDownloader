ec868edd5af023319db9776429c037a7b6ed3e74
Jazer <saltanhayrliev2006@gmail.com>
2025-08-21 22:49:46 +0300

    добавил дурацкую библиотеку потом перепишу

--- VideoDownloader/BotHandler.cs
+++ VideoDownloader/BotHandler.cs
@@ -1,10 +1,14 @@
 using Telegram.Bot;
 using Telegram.Bot.Types;
+using YoutubeExplode;
+using YoutubeExplode.Videos.Streams;
 
 namespace VideoDownloader;
 
 public class BotHandler
 {
+    private readonly YoutubeClient _youtubeClient = new YoutubeClient();
+    
     public async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update,
         CancellationToken cancellationToken)
     {
@@ -20,6 +24,21 @@ public class BotHandler
                     await botClient.SendMessage(chatId, "привет я твой помощник в скачивании видео из Ютуб",
                         cancellationToken: cancellationToken);
                     break;
+                default:
+                    var manifest = await _youtubeClient.Videos.Streams.GetManifestAsync(message.Text ?? string.Empty, cancellationToken);
+                    var streamInfo = manifest.GetMuxedStreams()
+                        .Where(s => s.Container == Container.Mp4)
+                        .GetWithHighestVideoQuality();
+                    await using (var stream =
+                                 await _youtubeClient.Videos.Streams.GetAsync(streamInfo, cancellationToken))
+                    {
+                        using var memoryStream = new MemoryStream();
+                        await stream.CopyToAsync(memoryStream, cancellationToken);
+                        memoryStream.Position = 0;
+                        
+                        await botClient.SendVideo(chatId, new InputFileStream(memoryStream, "video.mp4"));
+                        break;
+                    }
             }
         }
         catch (Exception e)
